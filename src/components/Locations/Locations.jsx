36a5c1b4180df1b206d4fa20caf4d6ca8583db1a
Erick Mireles <emireles.rosas@gmail.com>
2024-01-19 14:06:47 -0600

    Update Locations component styling and data

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -1,15 +1,43 @@
-import "locations.css";
 import { createSignal } from "solid-js";
+import "./Locations.css";
+import locationsData from "./LocationsData.json";
+import rappiLocationsData from "./RappiLocationsData.json";
 
 export default function Locations() {
+  const [selectedLocation, setSelectedLocation] = createSignal(
+    Object.keys(locationsData)[0]
+  );
+
+  console.log(locationsData[selectedLocation()]);
   return (
     <div class="locations-wrapper">
+      {/* Locations lists to handle tab changes */}
       <div class="locations-list-holder">
-        <ul>
-          <li>Parque Centro (Saltillo)</li>
-          <li>Monterrey (Próximamente)</li>
+        <p class="subheading">Sucusales Físicas</p>
+        <ul class="locations-list">
+          {Object.keys(locationsData).map((key) => {
+            return (
+              <li>
+                <button
+                  class={`location-select ${
+                    selectedLocation() === key ? "selected" : ""
+                  }`}
+                  onClick={() => setSelectedLocation(key)}>
+                  {locationsData[key].name}
+                </button>
+              </li>
+            );
+          })}
         </ul>
-        <ul></ul>
+        <p class="subheading">Monterrey solo por Rappi en:</p>
+        <ul class="locations-list secondary-locations">
+          {rappiLocationsData.map((location) => {
+            return <li>{location}</li>;
+          })}
+        </ul>
+      </div>
+      <div class="location-holder">
+        {() => <p>{locationsData[selectedLocation()].name}</p>}
       </div>
     </div>
   );
