7862068c14abe347c3b004f7ebd3079a2bc24b71
Erick Mireles <emireles.rosas@gmail.com>
2024-01-29 12:21:45 -0600

    Add new images and update CSS styles

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -8,7 +8,6 @@ export default function Locations() {
     Object.keys(locationsData)[0]
   );
 
-  console.log(locationsData[selectedLocation()]);
   return (
     <div class="locations-wrapper">
       {/* Locations lists to handle tab changes */}
@@ -23,7 +22,7 @@ export default function Locations() {
                     selectedLocation() === key ? "selected" : ""
                   }`}
                   onClick={() => setSelectedLocation(key)}>
-                  {locationsData[key].name}
+                  {locationsData[key].nombre}
                 </button>
               </li>
             );
@@ -36,9 +35,48 @@ export default function Locations() {
           })}
         </ul>
       </div>
-      <div class="location-holder">
-        {() => <p>{locationsData[selectedLocation()].name}</p>}
-      </div>
+
+      {() => (
+        <div class="location-holder">
+          <div class="location-info-wrapper">
+            <div class="location-info-holder">
+              <h5>Dirección</h5>
+              <p class="location-info">
+                {locationsData[selectedLocation()].dirección}
+              </p>
+            </div>
+            <div class="location-info-holder">
+              <h5>Teléfono</h5>
+              <p class="location-info">
+                {locationsData[selectedLocation()].teléfono}
+              </p>
+            </div>
+            <div class="location-info-holder">
+              <h5>Horario</h5>
+              {() =>
+                locationsData[selectedLocation()].horarios.map((horario) => {
+                  return <p class="location-info">{horario}</p>;
+                })
+              }
+            </div>
+            <iframe
+              src={locationsData[selectedLocation()].mapa}
+              width="100%"
+              height="240"
+              style="border:0;"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div class="location-img-holder">
+            <img
+              src={locationsData[selectedLocation()].img.src}
+              alt={locationsData[selectedLocation()].img.alt}
+              class="location-img"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
