3b855fb736b9a564bb3c151582eb64c8357545bb
Erick Mireles <emireles.rosas@gmail.com>
2024-06-01 14:21:09 -0600

    Start implementation for new design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,15 +3,19 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        body: ["Objectivity", "sans-serif"],
+      },
       colors: {
-        "dark-1": "#0B0B0A",
-        "dark-2": "#1B1A18",
-        "dark-3": "#363430",
-        "light-1": "#FAF8F4",
-        "light-2": "#C3BBAC",
-        "brand-1": "#FFC247",
+        "dark": "#1F1F1F",
+        "light": "#F8FCFA",
+        "brand": "#A2D8C8",
+        "brand-2": "#1F473B"
+      },
+      spacing: {
+        "page": "var(--page-spacing)",
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
