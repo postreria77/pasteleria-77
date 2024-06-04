7ba898f5059a5618b1a3257d1ef0c3d1e178cdb0
Erick Mireles <emireles.rosas@gmail.com>
2024-06-03 20:04:30 -0600

    Start work on marquee animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,19 +7,28 @@ export default {
         body: ["Objectivity", "sans-serif"],
       },
       colors: {
-        "dark": "#1F1F1F",
-        "light": "#F8FCFA",
-        "brand": "#A2D8C8",
+        dark: "#1F1F1F",
+        light: "#F8FCFA",
+        brand: "#A2D8C8",
         "brand-2": "#1F473B",
-        "brand-3": "#49AB8E"
+        "brand-3": "#49AB8E",
       },
       spacing: {
-        "page": "var(--page-spacing)",
+        page: "var(--page-spacing)",
       },
       backgroundImage: {
-        "hero": "url('/images/header-bg.png')",
+        hero: "url('/images/header-bg.png')",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(calc(-100% - 16px))" },
+        },
+      },
+      animation: {
+        marquee: "marquee 20s linear infinite",
       },
     },
-    plugins: [],
   },
+  plugins: [],
 };
