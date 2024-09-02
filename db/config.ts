aadd53f73b82283c93c9f12d162313dd0f55727f
Erick Mireles <emireles.rosas@gmail.com>
2024-09-01 23:15:33 -0400

    Add 'nuevo' property to db schema

--- db/config.ts
+++ db/config.ts
@@ -4,20 +4,20 @@ const Pasteles = defineTable({
   columns: {
     name: column.text(),
     description: column.text(),
-    image: column.text({ default: ""}),
+    image: column.text({ default: "" }),
     tradicional: column.boolean(),
     anytime: column.boolean(),
     rebanadaAnytime: column.boolean(),
     nuevo: column.boolean({ default: false }),
-  }
-})
+  },
+});
 
 const Categories = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
     name: column.text(),
-  }
-})
+  },
+});
 
 const Productos = defineTable({
   columns: {
@@ -26,15 +26,16 @@ const Productos = defineTable({
     price: column.number(),
     image: column.text(),
     categoryId: column.number({ references: () => Categories.columns.id }),
-  }
-})
+    nuevo: column.boolean({ default: false }),
+  },
+});
 
 const Bebidas = defineTable({
-    columns: {
-      name: column.text(),
-      price: column.number(),
-    }
-})
+  columns: {
+    name: column.text(),
+    price: column.number(),
+  },
+});
 
 export default defineDb({
   tables: { Pasteles, Productos, Categories, Bebidas },
